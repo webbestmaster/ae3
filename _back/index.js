094263174bfd7505cfd2fe8ab56f8eb04d093573
Turovtsov Dmitry <web.best.master@gmail.com>
2017-03-22 16:14:08 +0300

    add fs-server

---
+++ _back/index.js
@@ -0,0 +1,12 @@
+const FsServer = require('fs-server');
+
+const HTTP_PORT = process.env.PORT || 3000;
+
+const fsServerConfig = {
+    root: './_front/', // path to front-end part of site
+    port: HTTP_PORT // optional, recommended this, or do not use this field
+    // page404: 'custom-404-page/index.html' // optional, path to custom 404 page
+};
+
+const fsServer = new FsServer(fsServerConfig) // create server with config
+        .run(); // create server with config and run
