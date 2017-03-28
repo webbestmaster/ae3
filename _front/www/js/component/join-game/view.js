dac2e15fe677c6fbcc4a757a405b519d2e764bac
Turovtsov Dmitry <web.best.master@gmail.com>
2017-03-28 22:32:29 +0300

    eslint, join to room - in progress...

--- _front/www/js/component/join-game/view.js
+++ _front/www/js/component/join-game/view.js
@@ -2,19 +2,28 @@ import React, {Component, PropTypes} from 'react';
 import BaseView from './../../core/base-view';
 import {connect} from 'react-redux';
 import api from './../../api';
+import {Link} from 'react-router';
+import appConst from './../../const';
+
 // import ajax from './../../lib/internal/ajax';
 // import {Link} from 'react-router';
 // import appConst from './../../const';
-// import {userModel} from './../../api/user-model';
+import {userModel} from './../../api/user-model';
 
 class JoinGame extends BaseView {
 
     componentDidMount() {
 
-        api.getAvailableRooms().then(data => {
-            console.log('rooms id is here');
-            console.log(data);
+        api.getAvailableRooms().then(roomIds => {
+            console.warn('rooms id is here');
+
+
+
+            userModel.connectToRoom(roomIds[0]);
+
         });
+
+
 
     }
 
@@ -22,8 +31,12 @@ class JoinGame extends BaseView {
 
         return <div>
 
+
+
             <h1>get available rooms</h1>
             <h1>__join_game__</h1>
+
+            <Link to={appConst.link.offerGame}> __join_to_game__ </Link>
 
         </div>;
 
