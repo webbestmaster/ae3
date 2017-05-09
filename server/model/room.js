c4706a2b059daf71673a8bfff6e0bed2b7ec706b
Dmitry Turovtsov <web.best.master@gmail.com>
2017-05-09 15:20:20 +0300

    in progress...

--- server/model/room.js
+++ server/model/room.js
@@ -1,26 +1,26 @@
-/**
- * Created by dim on 23.4.17.
- */
-
 const BaseModel = require('./../base/base-model');
 const UserData = require('./user-data').model;
-
 const rooms = {};
 const generateId = require('./../lib/generate-id');
 const _ = require('lodash');
 const Chat = require('./chat').model;
 const sha1 = require('sha1');
-const createError = require('./../http/api').createError;
 const mapGuide = require('./../../www/maps/map-guide.json');
 const GameModel = require('./../../www/main/game/').model;
 
-const props = {
+const util = require('./../http/util.js');
+const {mask, checkType, createError} = util;
+const {props, createChecker} = checkType;
+const {createMask} = mask;
+
+const attr = {
     initialData: 'initial-data',
     game: 'game',
-    userIds: 'user-ids',
     usersData: 'users-data',
     chat: 'chat'
 };
+
+// ---> userIds
 
 class Room extends BaseModel {
 
@@ -28,117 +28,162 @@ class Room extends BaseModel {
         super();
 
         const room = this;
-
-        room.set({
-            [props.initialData]: gameData,
-            [props.userIds]: [],
-            [props.usersData]: [],
-            [props.chat]: new Chat()
-        });
-
-        room.get(props.initialData).unitLimit = mapGuide.settings.unitLimitList[0];
-        room.get(props.initialData).defaultMoney = mapGuide.settings.defaultMoneyList[0];
-
         const id = 'room-id-' + generateId();
 
-        room.set({id});
-
         rooms[id] = room;
+
+        room.set({
+            id,
+            [attr.initialData]: gameData,
+            [attr.usersData]: [],
+            [attr.chat]: new Chat()
+        });
+
+        room.get(attr.initialData).unitLimit = mapGuide.settings.unitLimitList[0];
+        room.get(attr.initialData).defaultMoney = mapGuide.settings.defaultMoneyList[0];
     }
 
     destroy() {
         const room = this;
-
         const roomId = room.get('id');
 
         Reflect.deleteProperty(rooms, roomId);
 
-        room.get(props.chat).destroy();
-        room.get(props.usersData).forEach(userData => userData.destroy());
+        room.get(attr.chat).destroy();
+        room.get(attr.usersData).forEach(userData => userData.destroy());
 
-        if (room.get(props.game)) {
-            room.get(props.game).destroy();
+        if (room.get(attr.game)) {
+            room.get(attr.game).destroy();
         }
 
-        console.log(roomId, 'destroyed');
-
         super.destroy();
+
+        console.log(roomId, 'destroyed');
     }
 
     // api methods
-    addChatMessage(req, res, pubUserId, params) {
+    addUserId(req, res, pubUserId, params) {
+        const room = this;
+        const usersData = room.get(attr.usersData);
+
+        if (_.find(usersData, item => item.getUserId() === pubUserId)) {
+            createError({}, res, 'User already exists, pubUserId:' + pubUserId);
+            return;
+        }
+
         res.end();
-        this.get(props.chat).addMessage(pubUserId, params.text);
+
+        const userData = new UserData();
+
+        userData.setUserId(pubUserId);
+        usersData.push(userData);
+    }
+
+    addChatMessage(req, res, pubUserId, params) {
+        const schema = {
+            props: {
+                text: {
+                    type: props.str,
+                    isRequired: true
+                }
+            }
+        };
+        const dryMask = createMask(schema);
+        const check = createChecker(schema);
+        const dryRequest = dryMask(params);
+
+        if (check(dryRequest).isInvalid) {
+            createError({}, res, 'Invalid message');
+            return;
+        }
+
+        res.end();
+        this.get(attr.chat).addMessage(pubUserId, params.text);
     }
 
     getState(req, res, pubUserId, params) {
         const room = this;
 
         res.end(JSON.stringify({
-            game: room.get(props.game) && room.get(props.game).getState(),
-            chatMessages: room.get(props.chat).getAllMessages(),
-            usersData: room.get(props.usersData).map(userData => userData.toJSON()),
-            unitLimit: room.get(props.initialData).unitLimit,
-            defaultMoney: room.get(props.initialData).defaultMoney
+            game: room.get(attr.game) && room.get(attr.game).getState(),
+            chatMessages: room.get(attr.chat).getAllMessages(),
+            usersData: room.get(attr.usersData).map(userData => userData.toJSON()),
+            unitLimit: room.get(attr.initialData).unitLimit,
+            defaultMoney: room.get(attr.initialData).defaultMoney
         }));
     }
 
-    addUserId(req, res, pubUserId, params) {
-        res.end();
+    updateUserData(req, res, pubUserId, params) {
+        const schema = {
+            props: {
+                teamId: {
+                    type: props.str
+                },
+                color: {
+                    type: props.str
+                }
+            }
+        };
+        const dryMask = createMask(schema);
+        const check = createChecker(schema);
+        const dryRequest = dryMask(params);
 
-        const room = this;
-        const userIds = room.get(props.userIds);
-
-        if (userIds.indexOf(pubUserId) !== -1) {
+        if (check(dryRequest).isInvalid) {
+            createError({}, res, 'Invalid new user params');
             return;
         }
 
-        userIds.push(pubUserId);
-
-        const userData = new UserData();
-
-        userData.setUserId(pubUserId);
-        room.get(props.usersData).push(userData);
-    }
-
-    updateUserData(req, res, pubUserId, params) {
-        res.end();
-
         const room = this;
-        const userData = _.find(room.get(props.usersData), item => pubUserId === item.getUserId());
+        const userData = _.find(room.get(attr.usersData), item => pubUserId === item.getUserId());
 
         if (userData) {
-            userData.set(_.pick(params, ['teamId', 'color']));
+            userData.set(dryRequest);
             res.end();
             return;
         }
 
-        createError({}, res, 'Can not find userData with userId ' + pubUserId);
+        createError({}, res, 'Can not find userData with userId: ' + pubUserId);
     }
 
     setInitialGameData(req, res, pubUserId, params) {
-        res.end();
+        const schema = {
+            props: {
+                unitLimit: {
+                    type: props.nbr
+                },
+                defaultMoney: {
+                    type: props.nbr
+                }
+            }
+        };
+        const dryMask = createMask(schema);
+        const check = createChecker(schema);
+        const dryRequest = dryMask(params);
 
-        const newParams = _.pick(params, ['unitLimit', 'defaultMoney']);
-        const room = this;
-
-        Object.assign(room.get(props.initialData), newParams);
-    }
-
-    startGame(req, res, pubUserId, params) {
-        res.end();
-
-        const room = this;
-
-        if (room.get(props.game)) {
-            console.log('game is exists');
+        if (check(dryRequest).isInvalid) {
+            createError({}, res, 'Invalid params for setInitialGameData');
             return;
         }
 
+        res.end();
+
+        Object.assign(this.get(attr.initialData), dryRequest);
+    }
+
+    startGame(req, res, pubUserId, params) {
+        const room = this;
+
+        if (room.get(attr.game)) {
+            createError({}, res, 'Game already started');
+            return;
+        }
+
+        res.end();
+
         const game = new GameModel();
 
-        game.setMap(room.get(props.initialData).map);
-        room.set(props.game, game);
+        game.setMap(room.get(attr.initialData).map);
+        room.set(attr.game, game);
     }
 
 }
