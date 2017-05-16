1ea34e9874ae3f7e77e01298fbd556ca1ac1db17
dmitry.turovtsov <dmitry.turovtsov@synesis.ru>
2017-05-16 21:03:59 +0300

    refactoring...

--- www/js/user/reducer.js
+++ www/js/user/reducer.js
@@ -1,59 +1,9 @@
 import {combineReducers} from 'redux';
+import createReducer from './../lib/create-reducer';
 const userConst = require('./const.json');
 
-const idState = (() => {
-    const initialState = {
-        id: 'user-id-' + Math.random() + '-not-defined'
-    };
-
-    return (state = initialState, {type, payload}) => {
-        if (type === userConst.type.setId) {
-            return {
-                ...state,
-                id: payload.id
-            };
-        }
-
-        return state;
-    };
-})();
-
-const roomIdState = (() => {
-    const initialState = {
-        roomId: 'room-id-is-not-defined'
-    };
-
-    return (state = initialState, {type, payload}) => {
-        if (type === userConst.type.setRoomId) {
-            return {
-                ...state,
-                roomId: payload.roomId
-            };
-        }
-
-        return state;
-    };
-})();
-
-const publicIdState = (() => {
-    const initialState = {
-        publicId: 'public-id-is-not-defined'
-    };
-
-    return (state = initialState, {type, payload}) => {
-        if (type === userConst.type.setPublicId) {
-            return {
-                ...state,
-                publicId: payload.publicId
-            };
-        }
-
-        return state;
-    };
-})();
-
 export default combineReducers({
-    idState,
-    roomIdState,
-    publicIdState
+    idState: createReducer({id: 'user-id-' + Math.random() + '-not-defined'}, userConst.type.setId),
+    roomIdState: createReducer({roomId: 'room-id-is-not-defined'}, userConst.type.setRoomId),
+    publicIdState: createReducer({ publicId: 'public-id-is-not-defined'}, userConst.type.setPublicId)
 });
