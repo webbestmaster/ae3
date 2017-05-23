7326e8ce0d8c64649430456fa47d1a2a68ce0551
dmitry.turovtsov <dmitry.turovtsov@synesis.ru>
2017-05-23 19:12:55 +0300

    rename

--- www/js/game/render/index.js
+++ www/js/game/render/index.js
@@ -10,4 +10,17 @@ class Render extends BaseModel {
     }
 }
 
+const loader = new PIXI.loaders.Loader();
+loader.add('assets/terrain.json');
+
+loader.load((loader, resources) => {
+    // resources is an object where the key is the name of the resource loaded and the value is the resource object.
+    // They have a couple default properties:
+    // - `url`: The URL that the resource was loaded from
+    // - `error`: The error that happened when trying to load (if any)
+    // - `data`: The raw data that was loaded
+    // also may contain other properties based on the middleware that runs.
+    const texture = PIXI.Texture.fromFrame("angle-road-1.png");
+});
+
 export {Render};
