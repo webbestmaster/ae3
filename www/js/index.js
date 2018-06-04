f6d92650bb0f366d17a0c46aea77e0b112a0f7d7
webbestmaster <web.best.master@gmail.com>
2018-06-04 15:31:35 +0300

    update webpack.config.js

--- www/js/index.js
+++ www/js/index.js
@@ -3,7 +3,7 @@
 
 import React from 'react';
 import {render} from 'react-dom';
-import {BrowserRouter} from 'react-router-dom';
+import BrowserRouter from 'react-router-dom/BrowserRouter';
 import {Provider} from 'react-redux';
 import thunk from 'redux-thunk';
 import {applyMiddleware, combineReducers, createStore} from 'redux';
