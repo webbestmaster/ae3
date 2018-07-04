75eb4b23f773b90e14eb201137da0ce7cc488a43
webbestmaster <web.best.master@gmail.com>
2018-07-04 12:54:53 +0300

    add pixel to svg

---
+++ tools/pixel-art-to-svg/index.js
@@ -0,0 +1,54 @@
+const fs = require('fs'); // eslint-disable-line id-length
+const pixel = require('pixel');
+const svg = require('pixel-to-svg');
+
+const pathToSrc = 'png';
+const pathToSvg = 'svg';
+
+async function convert(imageName) {
+    return new Promise((resolve, reject) => {
+        console.log('start:', imageName);
+        pixel.parse(pathToSrc + '/' + imageName)
+            .then(images => {
+                const svgData = svg.convert(images[0])
+                    .replace( // rgba to rgb
+                        /fill="rgba\((\d{1,3}),(\d{1,3}),(\d{1,3}),(\d{1,3})\)"/gi,
+                        'fill="rgb($1,$2,$3)"'
+                    )
+                    .replace( // remove full black square
+                        /<path fill="rgb\(0,0,0\)"[\s\S]+?\/>/,
+                        ''
+                    )
+                    .replace( // remove <g/> tag
+                        /<g>|<\/g>/gi,
+                        ''
+                    );
+
+                const svgFileName = imageName.replace(/\.[\S]+?$/, '') + '.svg';
+
+                fs.writeFile(pathToSvg + '/' + svgFileName, svgData, {encoding: 'utf8'}, err => {
+                    if (err) {
+                        console.log('error:', imageName);
+                        console.error(err);
+                        reject();
+                        return;
+                    }
+                    console.log('done:', imageName);
+                    resolve();
+                });
+            });
+    });
+}
+
+fs.readdir(pathToSrc, async (err, files) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+
+    while (files.length) {
+        await convert(files.pop());
+    }
+});
+
+
