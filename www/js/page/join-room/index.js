349cc4db6682f7ade09ae130fab89d4fe92ac771
webbestmaster <web.best.master@gmail.com>
2018-07-12 18:55:21 +0300

    i18n

--- www/js/page/join-room/index.js
+++ www/js/page/join-room/index.js
@@ -23,11 +23,14 @@ import Locale from './../../components/locale';
 import {ButtonListWrapper} from '../../components/ui/button-list-wrapper';
 import buttonListWrapperStyle from '../../components/ui/button-list-wrapper/style.scss';
 import {ButtonLink} from '../../components/ui/button-link';
+import {Button} from '../../components/ui/button';
 import type {LocaleType} from './../../components/locale/reducer';
+import style from './style.scss';
 
 type StateType = {|
     // roomIds: Array<string>,
-    roomDataList: Array<RoomDataType> | null
+    roomDataList: Array<RoomDataType> | null,
+    isRoomsFetching: boolean
     // settings: AllRoomSettingsType,
     // userList: Array<ServerUserType>
 |};
@@ -48,13 +51,16 @@ class JoinRoom extends Component<PropsType, StateType> {
         const view = this;
 
         view.state = {
-            roomDataList: null
+            roomDataList: null,
+            isRoomsFetching: false
         };
     }
 
     async componentDidMount(): Promise<void> {
         const view = this;
         const {props, state} = view;
+
+        view.setState({isRoomsFetching: true});
 
         const getAllRoomIdsResult = await serverApi.getAllRoomIds();
         const roomDataList = [];
@@ -63,7 +69,10 @@ class JoinRoom extends Component<PropsType, StateType> {
             roomDataList.push(await getRoomState(getAllRoomIdsResult.roomIds[ii]));
         }
 
-        view.setState({roomDataList});
+        view.setState({
+            roomDataList,
+            isRoomsFetching: false
+        });
     }
 
     async joinRoom(roomId: string): Promise<void> {
@@ -73,17 +82,23 @@ class JoinRoom extends Component<PropsType, StateType> {
         const socketId = auth.socket.id;
         const userId = auth.user.id;
 
+        view.setState({isRoomsFetching: true});
+
         const roomState = await getRoomState(roomId);
 
         if (roomState === null || roomState.userList.length === roomState.maxUserSize) {
             console.error('roomState is null or room state is fool', roomState);
+            view.setState({isRoomsFetching: false});
             await view.componentDidMount();
             return;
         }
 
         const joinRoomResult = await serverApi.joinRoom(roomId, userId, socketId);
 
+        view.setState({isRoomsFetching: false});
+
         if (joinRoomResult.roomId === '') {
+            console.error('Did not join into room with id', roomId);
             return;
         }
 
@@ -105,7 +120,7 @@ class JoinRoom extends Component<PropsType, StateType> {
     render(): Node {
         const view = this;
         const {props, state} = view;
-        const {roomDataList} = state;
+        const {roomDataList, isRoomsFetching} = state;
 
         if (roomDataList === null) {
             return (
@@ -140,33 +155,35 @@ class JoinRoom extends Component<PropsType, StateType> {
             );
         }
 
+        const pageClassName = isRoomsFetching ? serviceStyle.disabled : '';
+
         return (
-            <Page>
+            <Page className={pageClassName}>
                 {JoinRoom.renderHeader()}
                 <div className={serviceStyle.grow_1}>
                     {roomDataList
                         .map((roomData: RoomDataType): Node => {
                             return (
-                                <p
-                                    style={{
-                                        padding: 10,
-                                        cursor: 'pointer'
-                                    }}
+                                <Button
                                     onClick={async (): Promise<void> => {
-                                        const result = view.joinRoom(roomData.roomId);
+                                        const result = await view.joinRoom(roomData.roomId);
                                     }}
+                                    className={style.open_room_item}
                                     key={roomData.roomId}
                                 >
-                                    {roomData.roomId}
-                                    {' - '}
-                                    {roomData.settings.map.meta[props.locale.name].name}
-                                    {' - '}
-                                    {roomData.userList.length}
-                                    {' '}
-                                    /
-                                    {' '}
-                                    {roomData.maxUserSize}
-                                </p>
+                                    <div className={style.right_arrow}>
+                                        &gt;&gt;
+                                    </div>
+                                    <p className={serviceStyle.ellipsis}>
+                                        {'['}
+                                        {roomData.userList.length}
+                                        {'/'}
+                                        {roomData.maxUserSize}
+                                        {'] '}
+                                        {roomData.settings.map.meta[props.locale.name].name}
+                                    </p>
+                                    <p/>
+                                </Button>
                             );
                         })}
                 </div>
