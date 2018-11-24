92165cb4ea4636f07f9d9807d77c9f5ddc88f747
webbestmaster <web.best.master@gmail.com>
2018-11-24 19:21:40 +0300

    bot - in progress...

--- www/js/component/game/model/bot-helper.js
+++ www/js/component/game/model/bot-helper.js
@@ -2,6 +2,52 @@
 
 import type {BotResultActionDataType} from './bot';
 
+type PointType = {|
+    +x: number,
+    +y: number,
+|};
+
+export type RawRateType = {|
+    +placeArmor: number,
+    +attack: {|
+        +damageGiven: number,
+        +damageReceived: number,
+    |},
+
+    +pathSizeToNearNoOccupiedBuilding: number, // castle or farm, less better, will stimulate unit to move ahead
+    +canRaiseSkeleton: boolean,
+
+    // farm
+    +canFixFarm: boolean,
+    +canOccupyEnemyFarm: boolean,
+    +canOccupyNoManFarm: boolean,
+    +canDestroyEnemyFarm: boolean,
+    // +canDestroyNoManFarm: boolean, // catapult can not destroy no man's farm
+
+    // castle
+    +canOccupyEnemyCastle: boolean,
+    +canOccupyNoManCastle: boolean,
+|};
+
+function getEndPoint(botResultActionData: BotResultActionDataType): PointType {
+    const {unit, moveAction} = botResultActionData;
+    const {action} = moveAction;
+
+    if (action) {
+        return {
+            x: action.to.x,
+            y: action.to.y,
+        };
+    }
+
+    const unitAttr = unit.getAttr();
+
+    return {
+        x: unitAttr.x,
+        y: unitAttr.y,
+    };
+}
+
 function rateMoveAction(botResultActionData: BotResultActionDataType): number {
     return Math.random();
 }
