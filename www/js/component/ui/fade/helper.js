cf7c0b6387c2f56f698a0df22d9c84cf51bf5638
webbestmaster <web.best.master@gmail.com>
2018-12-18 12:19:42 +0300

    bot - in progress...

--- www/js/component/ui/fade/helper.js
+++ www/js/component/ui/fade/helper.js
@@ -1,8 +1,26 @@
 // @flow
 
-/* global window, requestAnimationFrame, Event */
+/* global window, document, requestAnimationFrame, Event */
 
 export function forceWindowUpdate() {
     window.dispatchEvent(new Event('resize'));
-    requestAnimationFrame((): void => window.dispatchEvent(new Event('resize')));
+
+    const {body} = document;
+
+    if (!body) {
+        console.error('body is not define');
+        return;
+    }
+
+    body.style.cssText = ';-webkit-transform: rotateZ(0deg);';
+    body.offsetHeight;
+    body.style.cssText += ';-webkit-transform: none;';
+
+    requestAnimationFrame(() => {
+        window.dispatchEvent(new Event('resize'));
+
+        body.style.cssText = '-webkit-transform: rotateZ(0deg)';
+        body.offsetHeight;
+        body.style.cssText += ';-webkit-transform: none';
+    });
 }
