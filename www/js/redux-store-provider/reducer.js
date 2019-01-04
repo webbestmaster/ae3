00731523b83ae6777448b6ccac8c9b0ce62ccc48
webbestmaster <web.best.master@gmail.com>
2019-01-04 11:02:31 +0300

    bot - in progress...

--- www/js/redux-store-provider/reducer.js
+++ www/js/redux-store-provider/reducer.js
@@ -1,18 +1,16 @@
 // @flow
 
 import type {SystemType} from '../component/system/reducer/root';
-import {system} from '../component/system/reducer/root';
+export {system} from '../component/system/reducer/root';
 
 import type {AuthType} from '../component/auth/reducer';
-import {auth} from '../component/auth/reducer';
+export {auth} from '../component/auth/reducer';
 
 import type {LocaleType} from '../component/locale/reducer';
-import {locale} from '../component/locale/reducer';
+export {locale} from '../component/locale/reducer';
 
 import type {StoreType} from '../component/store/reducer';
-import {store} from '../component/store/reducer';
-
-export {auth, system, locale, store};
+export {store} from '../component/store/reducer';
 
 export type GlobalStateType = {|
     +auth: AuthType,
